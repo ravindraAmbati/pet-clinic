71d9d0c418914dddcfb026f495b7ad092cb48d04
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-16 08:55:05 +0530

    initial commit

---
+++ src/test/java/com/springbootapps/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapps.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
