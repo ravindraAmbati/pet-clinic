578af8526b5730895458786aced0dc2351e7005c
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-18 20:22:17 +0530

    load bootstrap data on startup, closes #16

---
+++ pet-clinic-web/src/main/java/com/springbootapps/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,43 @@
+package com.springbootapps.petclinic.bootstrap;
+
+import com.springbootapps.petclinic.model.Owner;
+import com.springbootapps.petclinic.model.Vet;
+import com.springbootapps.petclinic.services.Map.OwnerServiceMap;
+import com.springbootapps.petclinic.services.Map.VetServiceMap;
+import com.springbootapps.petclinic.services.OwnerService;
+import com.springbootapps.petclinic.services.VetService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        this.ownerService = new OwnerServiceMap();
+        this.vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) {
+        Owner owner = new Owner();
+        owner.setId(1L);
+        owner.setFirstName("Leo");
+        owner.setLastName("Novo");
+
+        ownerService.save(owner);
+
+        System.out.println("### " + ownerService.findById(1L) + " ###");
+
+        Vet vet = new Vet();
+        vet.setId(2L);
+        vet.setFirstName("Log");
+        vet.setLastName("Tech");
+
+        vetService.save(vet);
+
+        System.out.println("### " + vetService.findById(2L) + " ###");
+    }
+}
