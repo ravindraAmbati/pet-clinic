6eb44c6987c967c967e0eccef173498d6cc2249c
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-18 13:55:59 +0530

    refactor, closes #14

--- pet-clinic-web/src/main/java/com/springbootapps/petclinic/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springbootapps/petclinic/Controllers/VetController.java
@@ -3,10 +3,11 @@ package com.springbootapps.petclinic.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String getList() {
         return "vets/index";
     }
