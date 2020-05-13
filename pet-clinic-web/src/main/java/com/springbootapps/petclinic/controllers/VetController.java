5e8685493319a31bcdf47bf04d8cfb369fd83a72
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-13 20:29:38 +0530

    add end points for all possible requests, closes #97

--- pet-clinic-web/src/main/java/com/springbootapps/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springbootapps/petclinic/controllers/VetController.java
@@ -1,11 +1,15 @@
 package com.springbootapps.petclinic.controllers;
 
+import com.springbootapps.petclinic.model.Vet;
 import com.springbootapps.petclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,6 +24,12 @@ public class VetController {
         vetService.findAll().forEach(vet -> log.info(vet.toString()));
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api")
+    public @ResponseBody
+    Set<Vet> getJson() {
+        return vetService.findAll();
     }
 
     @RequestMapping("/find")
